fecc2eea3193bc7c5f311dbc31bcfa9c3c7633d8
alisha2806 <alishasharma361@gmail.com>
2025-08-06 14:36:43 +0400

    push

---
+++ react-practice/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      hello world I am trying to deploy this app via GCP Firebase and run CI/CD
+      on github actions!!
+    </div>
+  );
+};
+
+export default App;
